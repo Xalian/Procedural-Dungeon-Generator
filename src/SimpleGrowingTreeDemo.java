9276b5ca7c4d5c8de4ea62ed9f8e64c179431c32
Stephanie Lozier <Stephlozier@gmail.com>
2017-07-01 21:18:50 +1000

    simple class to display GrowingTree to the console

---
+++ src/SimpleGrowingTreeDemo.java
@@ -0,0 +1,31 @@
+import dungeon.CellState;
+import mazeGenerator.*;
+public class SimpleGrowingTreeDemo {
+    public static void main(String args[]){
+        CellState[][] maze = new CellState[20][20];
+        for(int row = 0; row < maze.length; row++){
+            for(int col = 0; col <maze[row].length; col++){
+                maze[row][col] = CellState.WALL;
+            }
+
+        }
+        MazeGen gen =  new GrowingTree(maze,GTtype.NEWEST,1,1);
+        gen.completeMaze();
+        StringBuilder output = new StringBuilder();
+        for(int row = 0; row < maze.length; row++){
+            for(int col = 0; col <maze[row].length; col++){
+                if(maze[row][col] == CellState.WALL){
+                    output.append("@");
+                }
+                else if(maze[row][col] == CellState.OPEN){
+                    output.append(".");
+                }
+                else if(maze[row][col] == CellState.OPEN){
+                    output.append("B");
+                }
+            }
+            System.out.println(output.toString());
+            output = new StringBuilder();
+        }
+    }
+}
