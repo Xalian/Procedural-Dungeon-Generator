785c83e74a555c9bd8cb26684df74fcd3f8a1b31
Stephanie Lozier <Stephlozier@gmail.com>
2017-07-16 15:08:32 +1000

    added and implemented disconnected

--- src/mazeGenerator/MazeGen.java
+++ src/mazeGenerator/MazeGen.java
@@ -1,5 +1,9 @@
 package mazeGenerator;
 import dungeon.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class MazeGen {
 
     protected boolean finished;
@@ -25,4 +29,19 @@ public abstract class MazeGen {
 
     public abstract void nextStep();
 
+    protected boolean disconnected(Point p){
+        if ((p.y > 1) && (map[p.y - 1][p.x] != CellState.WALL) && (p.y > 1 && map[p.y - 1][p.x] != CellState.ROOM_BORDER)) {
+            return false;
+        }
+        if ((p.y < map.length - 2) && (map[p.y + 1][p.x] != CellState.WALL) && (p.y < map.length - 2 && map[p.y + 1][p.x] != CellState.ROOM_BORDER)) {
+            return false;
+        }
+        if ((p.x < map[p.y].length - 2) &&( map[p.y][p.x + 1] != CellState.WALL) && (p.x < map[p.y].length - 2 && map[p.y][p.x + 1] != CellState.ROOM_BORDER)) {
+            return false;
+        }
+        if ((p.x > 1) && (map[p.y][p.x - 1] != CellState.WALL)&& (p.x > 1 && map[p.y][p.x - 1] != CellState.ROOM_BORDER)) {
+            return false;
+        }
+        return true;
+        }
 }
