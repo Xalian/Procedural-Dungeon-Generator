e940e38a80fd3824a4e7dc15410e1b35f6deb493
Stephanie Lozier <Stephlozier@gmail.com>
2017-07-16 15:11:10 +1000

    minor update to display and size

--- src/SimpleGrowingTreeDemo.java
+++ src/SimpleGrowingTreeDemo.java
@@ -2,7 +2,7 @@ import dungeon.CellState;
 import mazeGenerator.*;
 public class SimpleGrowingTreeDemo {
     public static void main(String args[]){
-        CellState[][] maze = new CellState[20][20];
+        CellState[][] maze = new CellState[40][40];
         for(int row = 0; row < maze.length; row++){
             for(int col = 0; col <maze[row].length; col++){
                 maze[row][col] = CellState.WALL;
@@ -14,13 +14,16 @@ public class SimpleGrowingTreeDemo {
         for(int row = 0; row < maze.length; row++){
             for(int col = 0; col <maze[row].length; col++){
                 if(maze[row][col] == CellState.WALL){
-                    output.append("@");
+                    output.append("W");
                 }
                 else if(maze[row][col] == CellState.OPEN){
                     output.append(".");
                 }
                 else if(maze[row][col] == CellState.ROOM_BORDER){
                     output.append("B");
+                }
+                else if(maze[row][col] == CellState.ROOM){
+                    output.append("R");
                 }
             }
             System.out.println(output.toString());
